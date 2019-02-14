544d264365dd27675102fce00f9724b6a93befed
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2019-02-13 23:20:24 -0800

    first commit

---
+++ Assets/Scripts/LevelDeath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelDeath : MonoBehaviour
+{
+    public GameObject youFell;
+    public GameObject LevelAudio;
+
+    void OnTriggerEnter()
+    {
+        StartCoroutine(YouFellOff());
+
+    }
+
+    IEnumerator YouFellOff()
+    {
+        youFell.SetActive(true);
+        LevelAudio.SetActive(false);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(0);
+    }
+
+
+}
---
+++ Assets/Scripts/PlatformGripper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGripper : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject theLedge;
+
+    void OnTriggerEnter()
+    {
+        thePlayer.transform.parent = theLedge.transform;
+    }
+
+    void OnTriggerExit()
+    {
+        thePlayer.transform.parent = null;
+    }
+}
